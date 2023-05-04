bb77d9b36b289ff363bb11f35f57b4b98e5c70fe
Victoria Nygård <112623372+vickynygaard@users.noreply.github.com>
2023-05-04 17:28:35 +0200

    endret kode

--- funfacts/funfacts_test.go
+++ funfacts/funfacts_test.go
@@ -18,8 +18,7 @@ func TestGetFunFacts(t *testing.T) {
 		want  []string
 	}{
 		{input: "sun", want: []string{"Temperatur på ytre lag av Solen 5778°K", "Temperatur i Solens kjerne 15000000°C"}},
-		{input: "luna", want: []string{"Temperatur på Månens overflate om natten -183°C", "Temperatur på Månens overflate om dagen 
-106°C"}},
+		{input: "luna", want: []string{"Temperatur på Månens overflate om natten -183°C", "Temperatur på Månens overflate om dagen 106°C"}},
 		{input: "terra", want: []string{"Høyeste temperatur målt på Jordens overflate 56.7°C", "Laveste temperatur målt på Jordens overflate -89.4°C"}},
 		{input: "invalid", want: []string{"Sorry, I don't have any fun facts about that."}},
 	}
